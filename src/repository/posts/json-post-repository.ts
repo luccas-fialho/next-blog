c9857f7e294d0594677a397fe1ca4c78d2a4759b
Luccas Fialho <luccas.flh@gmail.com>
2025-07-21 10:01:55 -0300

    feat: post findAll and findById implementation

--- src/repository/posts/json-post-repository.ts
+++ src/repository/posts/json-post-repository.ts
@@ -1,14 +1,30 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./posts-repository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
 
 const ROOT_DIR = process.cwd();
+const JSON_PATH = resolve(ROOT_DIR, "src", "db", "seed", "posts.json");
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk() {
+    const json = await readFile(JSON_PATH, "utf-8");
+    const parsedJson = JSON.parse(json);
+    const { posts } = parsedJson;
+    return posts;
+  }
 
-  findAll(): Promise<PostModel[]> {}
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) throw new Error("Post not found!");
+
+    return post;
+  }
 }
-
-export const postRepository = new JsonPostRepository();
-
-console.log(ROOT_DIR);
