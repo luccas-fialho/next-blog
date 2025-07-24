1448362a876e5f86a1e173626c695c1dca124698
Luccas Fialho <luccas.flh@gmail.com>
2025-07-24 14:16:19 -0300

    style: rounded box notfound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div
       className={clsx(
-        "min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8",
+        "min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl",
         "dark:bg-slate-100 dark:text-slate-900",
         "flex items-center justify-center text-center"
       )}
