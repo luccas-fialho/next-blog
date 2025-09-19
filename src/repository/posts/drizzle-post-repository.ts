45189f70cf9d5dae464a16bf1149487b2d1bb2de
Luccas Fialho <luccas.flh@gmail.com>
2025-09-19 14:30:51 -0300

    feat: switch to drizzle repo

--- src/repository/posts/drizzle-post-repository.ts
+++ src/repository/posts/drizzle-post-repository.ts
@@ -11,16 +11,44 @@ export class DrizzlePostRepository implements PostRepository {
     return posts;
   }
 
-  async findBySlugPublic(slug: string): Promise<PostModel> {}
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.slug, slug), eq(posts.published, true)),
+    });
 
-  async findAll(): Promise<PostModel[]> {}
+    if (!post) throw new Error("Post not found for this slug!");
 
-  async findById(id: string): Promise<PostModel> {}
+    return post;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    if (!post) throw new Error("Post not found for this ID!");
+
+    return post;
+  }
 }
 
-(async () => {
-  const repo = new DrizzlePostRepository();
-  const posts = await repo.findAllPublic();
+// como-a-tecnologia-impacta-nosso-bem-estar false
+// os-desafios-do-trabalho-remoto-moderno true
+// 9eb8b7ac-2b48-4835-880a-a1c798e1a595 true
+// 6b204dab-2312-4525-820a-a0463560835f false
 
-  posts.forEach((post) => console.log(post.slug, post.published));
-})();
+// (async () => {
+//   const repo = new DrizzlePostRepository();
+//   const post = await repo.findById("6b204dab-2312-4525-820a-a0463560835f");
+//   console.log(post);
+
+//   //posts.forEach((post) => console.log(post.id, post.published));
+// })();
