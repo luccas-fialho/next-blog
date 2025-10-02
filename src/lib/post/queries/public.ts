fe55b908c5c870f8aa0b0475361000c86b5c0d34
Luccas Fialho <luccas.flh@gmail.com>
2025-10-02 11:18:19 -0300

    refactor: cache public posts functions

--- src/lib/post/queries/public.ts
+++ src/lib/post/queries/public.ts
@@ -3,25 +3,28 @@ import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
 import { cache } from "react";
 
-export const findAllPublicPostsCached = unstable_cache(
-  cache(async () => {
-    return await postRepository.findAllPublic();
-  }),
-  ["posts"],
-  { tags: ["posts"] }
+export const findAllPublicPostsCached = cache(
+  unstable_cache(
+    async () => {
+      return await postRepository.findAllPublic();
+    },
+    ["posts"],
+    {
+      tags: ["posts"],
+    }
+  )
 );
 
-export const findPostBySlugCached = (slug: string) =>
-  unstable_cache(
-    cache(async (slug: string) => {
+export const findPostBySlugCached = cache((slug: string) => {
+  return unstable_cache(
+    async (slug: string) => {
       const post = await postRepository
         .findBySlugPublic(slug)
         .catch(() => undefined);
       if (!post) notFound();
       return post;
-    }),
-    ["posts"],
-    {
-      tags: [`post-${slug}`],
-    }
+    },
+    [`post-${slug}`],
+    { tags: [`post-${slug}`] }
   )(slug);
+});
