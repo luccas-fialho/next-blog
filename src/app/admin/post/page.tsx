4dbdc1b0cb5f441a72f917144dfb457726f636ed
Luccas Fialho <luccas.flh@gmail.com>
2025-10-02 09:35:24 -0300

    feat: post admin page

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,7 +1,22 @@
+import { findAllPostAdmin } from "@/lib/post/queries/admin";
+import { Metadata } from "next";
+
 export const dynamic = "force-dynamic";
 
+export const metadata: Metadata = {
+  title: "Post Admin",
+};
+
 const AdminPostPage = async () => {
-  return <div>AdminPostPage</div>;
+  const posts = await findAllPostAdmin();
+
+  return (
+    <div className="py-16">
+      {posts.map((post) => {
+        return <p key={post.id}>{post.title}</p>;
+      })}
+    </div>
+  );
 };
 
 export default AdminPostPage;
