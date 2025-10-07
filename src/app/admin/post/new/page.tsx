990529a3ab6e496ef2df986ae4e639abf75ca5a0
Luccas Fialho <luccas.flh@gmail.com>
2025-10-07 14:38:53 -0300

    feat: button styles

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,7 +1,13 @@
+import Button from "@/components/Button";
+
 export const dynamic = "force-dynamic";
 
 const AdminNewPostPage = async () => {
-  return <div>AdminNewPostPage</div>;
+  return <div className="py-16 flex gap-4 flex-wrap">
+    <Button variant="default">It works like the JSX</Button>
+    <Button variant="ghost">It works like the JSX</Button>
+    <Button variant="danger">It works like the JSX</Button>
+  </div>;
 };
 
 export default AdminNewPostPage;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import React from "react";
+
+type ButtonVariants = "default" | "ghost" | "danger";
+type ButtonSizes = "sm" | "md" | "lg";
+
+type ButtonProps = {
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
+} & React.ComponentProps<"button">;
+
+const Button = ({
+  variant = "default",
+  size = "md",
+  ...props
+}: ButtonProps) => {
+  const buttonVariants = {
+    default: clsx("bg-blue-600 text-blue-100"),
+    ghost: clsx("bg-slate-200 text-slate-900"),
+    danger: clsx("bg-red-600 text-red-100"),
+  };
+
+  const buttonSizes = {
+    sm: clsx(),
+    md: clsx(),
+    lg: clsx(),
+  };
+
+  const buttonClasses = clsx(buttonVariants[variant], buttonSizes[size]);
+
+  return <button className={buttonClasses} {...props} />;
+};
+
+export default Button;
