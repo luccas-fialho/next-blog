7362c90f7e48992936a725a0da3c111aed3c5593
Luccas Fialho <luccas.flh@gmail.com>
2025-10-09 13:36:35 -0300

    feat: form component to manage posts

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,53 +1,13 @@
-import Button from "@/components/Button";
-import InputCheckbox from "@/components/InputCheckbox";
-import InputText from "@/components/InputText";
+import ManagePostForm from "@/components/ManagePostForm";
 
 export const dynamic = "force-dynamic";
 
 const AdminNewPostPage = async () => {
   return (
-    <form action="" className="mb-16">
-      <div className="flex flex-col gap-6">
-        <InputText placeholder="Type your name" labelText="Name" />
-
-        <InputText placeholder="Type your last name" labelText="Last Name" />
-
-        <InputCheckbox
-          placeholder="Type your last name"
-          labelText="Last Name"
-        />
-
-        <InputText
-          disabled
-          placeholder="Type your last name"
-          labelText="Last Name"
-          defaultValue={"xdxdxds"}
-        />
-
-        <InputText
-          disabled
-          placeholder="Type your last name"
-          labelText="Last Name"
-        />
-
-        <InputText
-          placeholder="Type your last name"
-          labelText="Last Name"
-          readOnly
-        />
-
-        <InputText
-          placeholder="Type your last name"
-          labelText="Last Name"
-          defaultValue={"hehee"}
-          readOnly
-        />
-
-        <div className="mt-6">
-          <Button type="submit">Submit</Button>
-        </div>
-      </div>
-    </form>
+    <>
+      <h1>Create post</h1>
+      <ManagePostForm />
+    </>
   );
 };
 
